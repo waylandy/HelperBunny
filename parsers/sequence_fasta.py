48231caaed3d5b115e1d8df93571b5e478ad7842
Wayland Yeung <waylandy2@gmail.com>
2021-06-20 13:31:54 -0400

    Update sequence_fasta.py

--- parsers/sequence_fasta.py
+++ parsers/sequence_fasta.py
@@ -8,8 +8,8 @@ def read_fasta(file):
     """
     is_header = lambda x: x.startswith('>')
     compress  = lambda x: ''.join(_.strip() for _ in x)
-    skip      = next(iter(groupby(open(file), is_header)))[0]
-    reader    = iter(groupby(open(file), is_header)) if skip else reader
+    reader    = iter(groupby(open(file), is_header))
+    reader    = iter(groupby(open(file), is_header)) if next(reader)[0] else reader
     for key, group in reader:
         if key:
             for header in group:
